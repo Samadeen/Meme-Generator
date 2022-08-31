296b4282ac4918c0a79263db4c61bdaff9c0ba24
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-08-31 17:59:21 +0100

    Fetched Image Url

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
+
 import './Meme.css';
+import Data from './MemeData';
 
 const Meme = () => {
+  const [memeImage, setNewImage] = useState();
+
+  const getMemeImage = () => {
+    const memeArr = Data.data.memes;
+    const ranNum = Math.floor(Math.random() * memeArr.length);
+
+    setNewImage(memeArr[ranNum].url);
+  };
+
   return (
     <main>
-      <form className='form'>
+      <div className='form'>
         <input text='text' className='form__input' placeholder='Top Text' />
         <input text='text' className='form__input' placeholder='Bottom Text' />
-        <button className='form__button'>Get a new meme image 📷</button>
-      </form>
+        <button onClick={getMemeImage} className='form__button'>
+          Get a new meme image 📷
+        </button>
+      </div>
+      <img src={memeImage} alt='Meme' className='form__image' />
     </main>
   );
 };
